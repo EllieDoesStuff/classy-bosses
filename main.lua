ff47c893fe3e45e81c873fb2b9efd71cf0f16457
EllieDoesStuff <ebakkar12@gmail.com>
2026-09-15 23:52:45 +0300

    haunt skin adding
    wahoo!!

--- main.lua
+++ main.lua
@@ -3,6 +3,9 @@ local mod = RegisterMod("classy-bosses", 1)
 local MONSTRO_TYPE = 20
 local MONSTRO_VARIANT = 0
 
+local HAUNT_TYPE = 260
+local HAUNT_VARIANT = 0
+
 function mod:OnMonstroInit(npc)
     if npc.Variant == MONSTRO_VARIANT then
         local sprite = npc:GetSprite()
@@ -17,5 +20,17 @@ function mod:OnMonstroInit(npc)
         end
     end
 end
-
 mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnMonstroInit, MONSTRO_TYPE)
+
+function mod:OnHauntInit(npc)
+    if npc.Variant == HAUNT_VARIANT then
+        local sprite = npc:GetSprite()
+        -- rookie ass elseif statements please ignore them </3
+        if math.random() < 0.1 then
+            
+            sprite:ReplaceSpritesheet(0, "gfx/bosses/rebirth/haunt_tophat.png")
+            sprite:LoadGraphics()
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnHauntInit, HAUNT_TYPE)
