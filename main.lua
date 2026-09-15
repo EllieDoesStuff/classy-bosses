c2fc8dc30116db1eda23502659cb64ea16512a65
EllieDoesStuff <ebakkar12@gmail.com>
2026-09-15 22:02:31 +0300

    First Initial Push
    Monstro now has a top hat and a pizza on his head lol

---
+++ main.lua
@@ -0,0 +1,21 @@
+local mod = RegisterMod("classy-bosses", 1)
+
+local MONSTRO_TYPE = 20
+local MONSTRO_VARIANT = 0
+
+function mod:OnMonstroInit(npc)
+    if npc.Variant == MONSTRO_VARIANT then
+        local sprite = npc:GetSprite()
+        -- rookie ass elseif statements please ignore them </3
+        if math.random() < 0.1 then
+            
+            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_tophat.png")
+            sprite:LoadGraphics()
+        elseif math.random() < 0.1 then
+            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_pizza.png")
+            sprite:LoadGraphics()
+        end
+    end
+end
+
+mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnMonstroInit, MONSTRO_TYPE)
