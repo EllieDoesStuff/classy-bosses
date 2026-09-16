2080b8de7051a638f57348ed48003c964ec1f730
EllieDoesStuff <ebakkar12@gmail.com>
2026-09-16 23:13:05 +0300

    json file saving stuff

--- main.lua
+++ main.lua
@@ -1,5 +1,18 @@
 local mod = RegisterMod("classy-bosses", 1)
 
+local json = require("json")
+
+local modData = {
+    skinChance = 0.1,
+    SkinMachineSpawnedThisRun = false
+}
+
+local function SaveGameData()
+    mod:SaveData(json.encode(modData))
+end
+mod:AddCallback(ModCallbacks.MC_PRE_GAME_EXIT, SaveGameData)
+mod:AddCallback(ModCallbacks.MC_POST_NEW_LEVEL, SaveGameData)
+
 local MONSTRO_TYPE = 20
 local MONSTRO_VARIANT = 0
 local MONSTRO_SUBTYPE = 0
@@ -32,9 +45,22 @@ local sfx = SFXManager()
 
 local SOUND_SPIN = Isaac.GetSoundIdByName("Spin")
 
+local skinChance = 0.1
+local displayText = ""
+local textDisplayTimer = 0
+
+local myFont = Font()
+myFont:Load("font/teammeatfont16.fnt")
+
 mod:AddCallback(ModCallbacks.MC_POST_GAME_STARTED, function(_, isContinued)
-    if not isContinued then
-        spawnedRoomIndexes = {}
+    if isContinued and mod:HasData() then
+        local loadedData = json.decode(mod:LoadData())
+        if loadedData then
+            modData.skinChance = loadedData.skinChance or 0.1
+            modData.SkinMachineSpawnedThisRun = loadedData.SkinMachineSpawnedThisRun or false
+        end
+    else
+        modData.SkinMachineSpawnedThisRun = false
     end
 end)
 
@@ -64,13 +90,20 @@ mod:AddCallback(ModCallbacks.MC_PRE_PLAYER_COLLISION, function(_, player, collid
         local data = collider:GetData()
         local game = Game()
 
-        if not data.TouchCooldown or game:GetFrameCount() >= data.TouchCooldown then
-            data.TouchCooldown = game:GetFrameCount() + 30 
+        if not data.TouchCooldown or game:GetFrameCount() >= data.TouchCooldown then --
+            data.TouchCooldown = game:GetFrameCount() + 30  --
             
             sprite:Play("Spin", true)
-            -- sfx:Play(SoundEffect.SOUND_COIN_SLOT)
             sfx:Play(SOUND_SPIN, 10)
-            -- put logic here
+            
+            modData.skinChance = math.floor((modData.skinChance + 0.1) * 10 + 0.5) / 10
+            
+            if modData.skinChance > 1.0 then
+                modData.skinChance = 0.0
+            end
+            
+            displayText = "Variant Chance Changed To: " .. tostring(modData.skinChance * 100) .. "%"
+            textDisplayTimer = 60
         end
 
         return false
@@ -102,8 +135,22 @@ mod:AddCallback(ModCallbacks.MC_POST_UPDATE, function(_)
             sprite:Play("Idle", true)
         end
     end
+    if textDisplayTimer > 0 then
+        textDisplayTimer = textDisplayTimer - 1
+    end
 end)
 
+mod:AddCallback(ModCallbacks.MC_POST_RENDER, function()
+    if textDisplayTimer > 0 then
+        local screenW = Isaac.GetScreenWidth()
+        local screenH = Isaac.GetScreenHeight()
+        
+        local renderX = screenW / 2.8
+        local renderY = screenH * 0.6
+        
+        myFont:DrawStringScaled(displayText, renderX, renderY, 0.4, 0.4, KColor(1,1,1,1), 0, true)
+    end
+end)
 
 
 -- rookie ass elseif statements please ignore them </3
@@ -111,31 +158,32 @@ end)
 -- monstro
 function mod:OnMonstroInit(npc)
     local sprite = npc:GetSprite()
-    if npc.Variant == MONSTRO_VARIANT and npc.SubType == MONSTRO_SUBTYPE then
-        if math.random() < 0.1 then
+    
+    if math.random() < modData.skinChance then
+        local rollVariant = math.random(1, 2)
+        
+        if npc.Variant == MONSTRO_VARIANT and npc.SubType == MONSTRO_SUBTYPE then
+            if rollVariant == 1 then
+                sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_tophat.png")
+            else
+                sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_pizza.png")
+            end
+            sprite:LoadGraphics()
             
-            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_tophat.png")
+        elseif npc.Variant == MONSTRO_VARIANT and npc.SubType == MONSTRO_SUBTYPE1 then
+            if rollVariant == 1 then
+                sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_red_tophat.png")
+            else
+                sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_red_pizza.png")
+            end
             sprite:LoadGraphics()
-        elseif math.random() < 0.1 then
-            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_pizza.png")
-            sprite:LoadGraphics()
-        end
-    elseif npc.Variant == MONSTRO_VARIANT and npc.SubType == MONSTRO_SUBTYPE1 then
-        if math.random() < 0.1 then
             
-            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_red_tophat.png")
-            sprite:LoadGraphics()
-        elseif math.random() < 0.1 then
-            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_red_pizza.png")
-            sprite:LoadGraphics()
-        end
-    elseif npc.Variant == MONSTRO_VARIANT and npc.SubType == MONSTRO_SUBTYPE2 then
-        if math.random() < 0.1 then
-            
-            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_black_tophat.png")
-            sprite:LoadGraphics()
-        elseif math.random() < 0.1 then
-            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_black_pizza.png")
+        elseif npc.Variant == MONSTRO_VARIANT and npc.SubType == MONSTRO_SUBTYPE2 then
+            if rollVariant == 1 then
+                sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_black_tophat.png")
+            else
+                sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_black_pizza.png")
+            end
             sprite:LoadGraphics()
         end
     end
@@ -146,19 +194,19 @@ mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnMonstroInit, MONSTRO_TYPE)
 function mod:OnHauntInit(npc)
     local sprite = npc:GetSprite()
     if npc.Variant == HAUNT_VARIANT and npc.SubType == HAUNT_SUBTYPE then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/rebirth/haunt_tophat.png")
             sprite:LoadGraphics()
         end
     elseif npc.Variant == HAUNT_VARIANT and npc.SubType == HAUNT_SUBTYPE1 then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/rebirth/haunt_black_tophat.png")
             sprite:LoadGraphics()
         end
     elseif npc.Variant == HAUNT_VARIANT and npc.SubType == HAUNT_SUBTYPE2 then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/rebirth/haunt_pink_tophat.png")
             sprite:LoadGraphics()
@@ -171,19 +219,19 @@ mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnHauntInit, HAUNT_TYPE)
 function mod:OnDukeInit(npc)
     local sprite = npc:GetSprite()
     if npc.Variant == DUKE_VARIANT and npc.SubType == DUKE_SUBTYPE then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/dukeofflies_sunglasses.png")
             sprite:LoadGraphics()
         end
     elseif npc.Variant == DUKE_VARIANT and npc.SubType == DUKE_SUBTYPE1 then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/dukeofflies_green_sunglasses.png")
             sprite:LoadGraphics()
         end
     elseif npc.Variant == DUKE_VARIANT and npc.SubType == DUKE_SUBTYPE2 then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/dukeofflies_orange_sunglasses.png")
             sprite:LoadGraphics()
@@ -196,13 +244,13 @@ mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnDukeInit, DUKE_TYPE)
 function mod:OnBloatInit(npc)
     local sprite = npc:GetSprite()
     if npc.Variant == BLOAT_VARIANT and npc.SubType == BLOAT_SUBTYPE then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/bloat_poo.png")
             sprite:LoadGraphics()
         end
     elseif npc.Variant == BLOAT_VARIANT and npc.SubType == BLOAT_SUBTYPE1 then
-        if math.random() < 0.1 then
+        if math.random() < modData.skinChance then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/bloat_green_poo.png")
             sprite:LoadGraphics()
