b7ec5002bc30df8715544d8d3a11797bcfdeb26a
EllieDoesStuff <ebakkar12@gmail.com>
2026-09-16 08:10:30 +0300

    duke of flies and bloat

--- main.lua
+++ main.lua
@@ -6,10 +6,18 @@ local MONSTRO_VARIANT = 0
 local HAUNT_TYPE = 260
 local HAUNT_VARIANT = 0
 
+local DUKE_TYPE = 67
+local DUKE_VARIANT = 0
+
+local BLOAT_TYPE = 68
+local BLOAT_VARIANT = 1
+
+-- rookie ass elseif statements please ignore them </3
+
+-- monstro
 function mod:OnMonstroInit(npc)
     if npc.Variant == MONSTRO_VARIANT then
         local sprite = npc:GetSprite()
-        -- rookie ass elseif statements please ignore them </3
         if math.random() < 0.1 then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/monstro_tophat.png")
@@ -22,10 +30,10 @@ function mod:OnMonstroInit(npc)
 end
 mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnMonstroInit, MONSTRO_TYPE)
 
+-- haunt
 function mod:OnHauntInit(npc)
     if npc.Variant == HAUNT_VARIANT then
         local sprite = npc:GetSprite()
-        -- rookie ass elseif statements please ignore them </3
         if math.random() < 0.1 then
             
             sprite:ReplaceSpritesheet(0, "gfx/bosses/rebirth/haunt_tophat.png")
@@ -34,3 +42,29 @@ function mod:OnHauntInit(npc)
     end
 end
 mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnHauntInit, HAUNT_TYPE)
+
+-- duke
+function mod:OnDukeInit(npc)
+    if npc.Variant == DUKE_VARIANT then
+        local sprite = npc:GetSprite()
+        if math.random() < 0.1 then
+            
+            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/dukeofflies_sunglasses.png")
+            sprite:LoadGraphics()
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnDukeInit, DUKE_TYPE)
+
+-- bloat
+function mod:OnBloatInit(npc)
+    if npc.Variant == BLOAT_VARIANT then
+        local sprite = npc:GetSprite()
+        if math.random() < 0.1 then
+            
+            sprite:ReplaceSpritesheet(0, "gfx/bosses/classic/bloat_poo.png")
+            sprite:LoadGraphics()
+        end
+    end
+end
+mod:AddCallback(ModCallbacks.MC_POST_NPC_INIT, mod.OnBloatInit, BLOAT_TYPE)
